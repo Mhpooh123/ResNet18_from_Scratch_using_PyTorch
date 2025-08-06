d8f2e901a80cb9c95137156ec5ead4cab6d2281d
Mhpooh123 <pop4399133@gmail.com>
2025-08-06 15:41:52 +0700

    add 2

--- testcudaandlibrary.py
+++ testcudaandlibrary.py
@@ -2,5 +2,4 @@ import torch
 print(torch.version.cuda) # เวอร์ชัน CUDA ที่ PyTorch ใช้
 print(torch.cuda.is_available())  # ถ้า False คือยังใช้ GPU ไม่ได้
 
-import numpy
-print(numpy.__version__)
+
